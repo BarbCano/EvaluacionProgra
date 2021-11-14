78fd1bf579065be3c70dc6681bde0d7bc2c30cdd
BarbCano <94261758+BarbCano@users.noreply.github.com>
2021-11-13 22:29:47 -0600

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-nombreGerente = input('Ingrese nombre del gerente\n')
-gerenton = Gerente(nombreGerente, 1.80, 'Gaberdina de 3 botones', 'Monse.inc', 'Gerente de surcursal')
-gerenton.saludar()
-gerenton.entrevistar('Jona')
